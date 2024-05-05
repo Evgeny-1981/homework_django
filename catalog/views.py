becf51b7610c9f77c5c0005f0f98a46be134612a
Evgeny Egorov <EAEgorov42@gmail.com>
2024-05-05 08:57:00 +0500

    Домашняя работа 19-2

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def home(request):
 
 def contact(request):
     """Контроллер отображения страницы с контактами"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}, телефон: {phone}, сообщение: {message}')
     return render(request, 'catalog/contacts.html')
