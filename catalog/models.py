b1591df968630102d02bafe552279f9287a3ca5c
Evgeny Egorov <EAEgorov42@gmail.com>
2024-05-08 08:11:19 +0500

    add "manufactured_at" and create migration and apply migration

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -24,6 +26,7 @@ class Product(models.Model):
     price = models.FloatField(verbose_name='цена за покупку')
     create_at = models.DateTimeField(verbose_name='дата создания')
     update_at = models.DateTimeField(verbose_name='дата последнего изменения')
+    manufactured_at = models.DateTimeField(verbose_name='дата производства продукта', default=datetime.datetime.now())
 
     def __str__(self):
         return f"Наименование продукта: {self.name}, категория продукта: {self.category}"
