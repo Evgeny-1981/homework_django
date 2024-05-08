9cbeaebc63a9cb78afce70c751788be223074002
Evgeny Egorov <EAEgorov42@gmail.com>
2024-05-08 13:56:23 +0500

    task 5 in home work 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
+
 class Category(models.Model):
     name = models.CharField(max_length=120, verbose_name='наименование')
     description = models.TextField(max_length=255, verbose_name='описание', **NULLABLE)
 
     def __str__(self):
-       return f"Наименование категории: {self.name}, описание: {self.description}"
-
+        return f"Наименование категории: {self.name}, описание: {self.description}"
 
     class Meta:
         verbose_name = 'категория'
@@ -26,14 +26,13 @@ class Product(models.Model):
     price = models.FloatField(verbose_name='цена за покупку')
     create_at = models.DateTimeField(verbose_name='дата создания')
     update_at = models.DateTimeField(verbose_name='дата последнего изменения')
-    #manufactured_at = models.DateTimeField(verbose_name='дата производства продукта', default=datetime.datetime.now())
+
+    # manufactured_at = models.DateTimeField(verbose_name='дата производства продукта', default=datetime.datetime.now())
 
     def __str__(self):
-        return f"Наименование продукта: {self.name}, категория продукта: {self.category}"
+        return f"Наименование продукта: {self.name}, категория продукта: {self.category}, цена: {self.price}"
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ('name',)
-
-
