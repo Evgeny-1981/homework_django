44cb335f2f6a4320a4c6f41fd0233508a4b3502b
Evgeny Egorov <EAEgorov42@gmail.com>
2024-05-11 12:01:01 +0500

    customs command task 6.3

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 
 NULLABLE = {"blank": True, "null": True}
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 def home(request):
     """Контроллер отображения домашней страницы"""
     return render(request, 'catalog/home.html')
+
 
 def contact(request):
     """Контроллер отображения страницы с контактами"""
