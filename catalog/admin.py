3d3b68eede22a73fc127655dcee08bdffafd8fae
Evgeny Egorov <EAEgorov42@gmail.com>
2024-05-25 13:07:00 +0500

    Home work Django - add slug for model Blog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Blog
 
 
 @admin.register(Category)
@@ -13,3 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'quantity', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'created_at', 'published',)
+    list_filter = ('created_at',)
+    search_fields = ('title', 'created_at', 'published',)
