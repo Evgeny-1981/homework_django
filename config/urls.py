39fb9d3fb21dcb49003d58dda0481d07336ea13e
Evgeny Egorov <EAEgorov42@gmail.com>
2024-06-21 12:12:22 +0500

    Home work Django 22.1 создана новая модель User, добавлена аутентификация, регистрация, редактирование профиля пользователя

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls'))
+    path('', include('catalog.urls')),
+    path('users/', include('users.urls', namespace='users'))
 
 ]
 
