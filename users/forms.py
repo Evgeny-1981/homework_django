c0739e826f8b3fc3fe4c8ea1896d92ed29cf417c
Evgeny Egorov <EAEgorov42@gmail.com>
2024-06-23 01:37:39 +0500

    home work 22.2 - all good

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from users.models import User
 from catalog.forms import FormMixin
 
@@ -13,9 +13,22 @@ class UserRegisterForm(FormMixin, UserCreationForm):
 class UserProfileForm(FormMixin, UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name',  'phone', 'avatar', 'country')
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['password'].widget = forms.HiddenInput()
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label="Старый пароль", widget=forms.PasswordInput(attrs={"class": "form_input"})
+    )
+    new_password1 = forms.CharField(
+        label="Новый пароль", widget=forms.PasswordInput(attrs={"class": "form_input"})
+    )
+    new_password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput(attrs={"class": "form_input"}),
+    )
